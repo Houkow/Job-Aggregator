110c2c33e1b0c85499a72e2dbffd7f3cc90dc802
Furkan <furkan.ozkan@epitech.eu>
2026-05-06 16:13:32 +0200

    ajout du middleware de validation de données

--- backend/src/routes/ingest.routes.js
+++ backend/src/routes/ingest.routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { exec } = require('child_process')
 
 router.post('/', (req, res) => {
-    exec('python3 ../data/ingestion/main.py', (err, stdout, stderr) => {
+    exec('python3 ../data/ingestion/api.py', (err, stdout, stderr) => {
         if (err) {
             console.error('Erreur ingestion :', err)
             return res.status(500).json({ message: 'Erreur lors de l ingestion' })
